3e12fd001d8b2dfa17cc1b32d57ad5b3f86174b0
hamza-burhan <hamzaburhan0985@gmail.com>
2024-12-27 16:57:53 +0500

    example of get http client

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,17 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
+
 use App\Models\User;
 
 
 class UserController extends Controller
 {
     public function getUser(){
-        //Example of db
-        $users = User::all();
+        // example of httpClient get
+        $response = Http::get('https://jsonplaceholder.typicode.com/users');
+        $users = $response->json();
         return $users;
     }
-    public function setuser($id){
-        return "Setting user with id: $id";
-    }
+
+    
 }
