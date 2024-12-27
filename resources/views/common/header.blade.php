31ad0297a32d974ed81d556e40a254803c6f6624
hamza-burhan <hamzaburhan0985@gmail.com>
2024-12-27 10:26:31 +0500

    example of subview

---
+++ resources/views/common/header.blade.php
@@ -0,0 +1,4 @@
+<div>
+   <h1>This is header</h1>
+   <h1>this is header and from user file {{$user}}</h1>
+</div>
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,3 +1,6 @@
+<!-- example of view -->
+@includeif('common.header', ['user' => $users[2]])
+
 @foreach($users as $user)
 <div>
     <h1>{{$user}}</h1>
