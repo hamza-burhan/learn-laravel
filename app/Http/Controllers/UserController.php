55fca1ecba154664167f531a0d214934f6abcb5f
hamza-burhan <hamzaburhan0985#gmail.com>
2024-12-28 17:51:25 +0500

    Modal example

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,35 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Student;
 
 class UserController extends Controller
 {
     public function getUser(){
 
-        //example of Create record
-        // $user = DB::table("students")->insert([
-        //     "name"=> "Burhan",
-        //     "dep" => "BD",
-        // ]);
-
-
-        //example of Read
-        // $user = DB::table("students")->get();
-
-
-        //exaple of Update
-        // $user = DB::table('students')->where('dep', 'CS')
-        // ->update(['name' => 'Update Hamza']);
-
-        //example of Delete
-        $user = DB::table('students')->where('name', 'Burhan')->delete();
-
-        
-        return $user;
-
-
-        //example of get records
-        // $users = DB::table("users")->get();
-        // dd($users);
+        //example with model
+        $student = Student::all();
+        return $student;
     }
 }
